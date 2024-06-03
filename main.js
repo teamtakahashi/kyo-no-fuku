3d2202f35e418d4202ec583bd91e5bcf69a2d0aa
ksSaka <sakakou.fukui@gmail.com>
2024-06-03 11:16:12 +0900

    気象庁APIテスト2

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
 document.write("hello world")
 document.write("こーれコミットできません")
 
-// 福井(180000)の予報を取得
-let url = "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json";
 
-fetch(url)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(weather) {
-        console.log(weather);
-    });
